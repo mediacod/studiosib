bc526ea25de76c29bdfe193807b70b7abe01fba4
Artem <am.nemtsov@mail.ru>
2021-08-10 15:28:21 +0500

    Добавить контроллеры account и Auth для сайта

---
+++ src/MediaStudio.Services/MediaStudio/MediaStudio/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+﻿namespace MediaStudio.Controllers
+{
+    using MediaStudioService.Models.Input;
+    using MediaStudioService.Services;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Newtonsoft.Json.Linq;
+
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthService authService;
+
+        public AuthController(AuthService authService)
+        {
+            this.authService = authService;
+        }
+
+        [HttpPost("SignIn")]
+        public object SignIn(InputAccount inputAccount)
+        {
+            return authService.GetToken(inputAccount, Request.HttpContext);
+        }
+
+        [HttpPost("Refresh")]
+        [AllowAnonymous]
+        public object Refresh([FromBody] JObject jsonValue)
+        {
+            var token = jsonValue["accessToken"].ToString();
+            var refreshToken = jsonValue["refreshToken"].ToString();
+
+            return authService.RefreshToken(token, refreshToken, Request.HttpContext);
+        }
+    }
+}
